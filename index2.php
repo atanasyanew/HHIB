0dc233dad33d24505afa3cacdc1908f6a6c54a03
A. Yanev <atanas.yanew@gmail.com>
2017-03-14 20:21:06 +0100

    test index2.php

---
+++ index2.php
@@ -0,0 +1,50 @@
+
+<div class="w3-border w3-margin">
+    <div class="w3-container w3-teal">
+        <h2>Начало</h2>  
+    </div>   
+</div>   <!-- header --> 
+
+
+
+<div class="w3-margin">  
+    
+<div class="w3-container w3-center">
+    <h2>Уеб-базирано приложение за управление</h2>     
+    
+    <P>
+    <?php  
+        
+echo "rabotiiiiiiiiiiiiiiiiiiiii";
+    ?>
+    </P> 
+    
+</div>  
+
+    
+</div>
+
+
+
+<div class="w3-margin w3-center">
+<!--    
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8RH8EbI2odhEnLkXJ7jKbkwi-9c2u9SMMID0AxPQHZF0OoQ/viewform?embedded=true" width="760" height="1800" frameborder="0" marginheight="0" marginwidth="0">Зарежда се…</iframe>  
+-->
+    
+    
+ <!--    
+<div class="w3-container w3-border">      
+<h2 class="w3-text-red">СИСТЕМНО СЪОБЩЕНИЕ!</h2>  
+<div class="w3-article">  
+<p>коментар</p>
+</div>
+</div>    
+ -->   
+    
+<br>  
+</div> 
+
+
+
+
+<?PHP include_layout_template('footer.php'); ?>
