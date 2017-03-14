740907cd9fa7520915dac7965dc91af77fe1105b
A. Yanev <atanas.yanew@gmail.com>
2017-03-14 19:27:12 +0100

    new db config settings

--- _Includes/config.php
+++ _Includes/config.php
@@ -1,6 +1,6 @@
 <?php
 // Database Constants
-defined('DB_SERVER') ? null : define("DB_SERVER", "MYSQLCONNSTR_localdb");
+defined('DB_SERVER') ? null : define("DB_SERVER", "127.0.0.1:55976");
 defined('DB_USER')   ? null : define("DB_USER", "hhib");
 defined('DB_PASS')   ? null : define("DB_PASS", "koea");
 defined('DB_NAME')   ? null : define("DB_NAME", "hhib");
